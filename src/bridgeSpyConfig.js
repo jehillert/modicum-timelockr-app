e5290a0b884ed0029793d2cd159d820ab91e0b15
John Hillert <john.hillert@gmail.com>
2021-01-05 00:13:09 -0600

    colorized bridgeSpyConfig.js

--- src/bridgeSpyConfig.js
+++ src/bridgeSpyConfig.js
@@ -2,17 +2,54 @@ import MessageQueue from 'react-native/Libraries/BatchedBridge/MessageQueue.js';
 
 const methodsToWatch = ['createCalendarEvent', 'getConstants'];
 
-// {
-//     type: int (0=N->JS | 1=JS->N)
-//     method: string,
-//     module: string,
-//     args: object,
-// }
+const colors = [
+    'color: orange',
+    'color: hotpink',
+    'color: white',
+    'color: hotpink',
+];
+
+const logDivider = () => {
+    console.log(
+        `%c———————————————————————————————————————————————————————————————————————————————`,
+        'color: hotpink',
+    );
+};
+
+const logMsgObj = msg => {
+    console.log('%c⧭', 'color: red', msg);
+};
+
+const logType = ({ type }) => {
+    console.log(`%ctype%c: %c${type}%c;`, ...colors);
+};
+
+const logModule = ({ module }) => {
+    console.log(`%cmodule%c: %c${module}%c;`, ...colors);
+};
+
+const logMethod = ({ method }) => {
+    console.log(`%cmethod%c: %c${method}%c;`, ...colors);
+};
+
+const logArgs = ({ args }) => {
+    console.log(
+        `%cargs%c: [%c${String(args).replace(/,/gi, '|')}%c];`,
+        ...colors,
+    );
+};
 
 export default function initializeBridgeSpy() {
     const spyFunction = msg => {
+        const { type = null, method = '', module = '', args = null } = msg;
         if (methodsToWatch.includes(msg.method)) {
-            console.log(msg);
+            logDivider();
+            // logType(msg);
+            logModule(msg);
+            logMethod(msg);
+            logArgs(msg);
+            logMsgObj(msg);
+            logDivider();
         }
     };
 
