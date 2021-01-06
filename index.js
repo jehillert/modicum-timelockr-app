504e38769f8a6c669b15a4cdb69006c3aaeeb997
John Hillert <john.hillert@gmail.com>
2021-01-05 22:56:48 -0600

    Fixed react-native-gesture-handler

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import 'react-native-gesture-handler';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 if (__DEV__) {
     import('./ReactotronConfig').then(() =>
         console.log('Reactotron Configured'),
@@ -8,4 +8,4 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
