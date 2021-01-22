45fe8cf2198d22bed15ff2796e594663589c12ca
John Hillert <john.hillert@gmail.com>
2021-01-22 01:44:29 -0600

    abc

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -2,10 +2,10 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { devicesReducer, serviceReducer, unlockReducer, unshackleReducer } from '@noke-state';
 
 const rootReducer = combineReducers({
-    unlock: unlockReducer,
-    unshackle: unshackleReducer,
     devices: devicesReducer,
     service: serviceReducer,
+    unlock: unlockReducer,
+    unshackle: unshackleReducer,
 });
 
 export default rootReducer;
