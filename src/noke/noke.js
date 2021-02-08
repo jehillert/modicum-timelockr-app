9ed8881cd98b83c51c2372a99208727c48916236
John Hillert <john.hillert@gmail.com>
2021-02-08 04:56:05 -0600

    remove observables from RNNoke

--- src/noke/noke.js
+++ src/noke/noke.js
@@ -1,75 +1,18 @@
-import { NativeEventEmitter, NativeModules } from 'react-native';
-import nokeConstants from './noke-constants';
+import { NativeModules } from 'react-native';
+import nokeConstants, { nokeEvents } from './noke-constants';
 import nokeUtils from './noke-utils';
-import { Observable } from 'rxjs';
 
 const { NokeAndroidMobileLibrary } = NativeModules;
 
-// EVENT UTILITIES
-const NokeEmitter = new NativeEventEmitter(NokeAndroidMobileLibrary);
-
-export const onEvent = function (eventName, callback) {
-    console.log(`eventName: ${eventName}`);
-    NokeEmitter.addListener(eventName, callback);
-    return this;
-};
-
-export const onEventOnce = function (eventName, callback) {
-    NokeEmitter.once(eventName, callback);
-    return this;
-};
-
-export const offEvent = function (eventName, listener) {
-    NokeEmitter.removeListener(eventName, listener);
-    return this;
-};
-
-export const getEventListeners = function (eventName) {
-    return NokeEmitter.listeners(eventName);
-};
-
-export const fromNokeEvents = () => {
-    if (!Observable) {
-        return {
-            message: 'Missing rxjs',
-        };
-    }
-
-    let lastEvent = '';
-
-    const { nokeEvents } = nokeConstants;
-
-    return new Observable(observer => {
-        Object.keys[nokeEvents].forEach(eventName => {
-            onEvent(eventName, data => {
-                observer.next({
-                    name: eventName,
-                    data,
-                });
-                lastEvent = eventName;
-            });
-        });
-    });
-};
-
-export { nokeEvents } from './noke-constants';
+export { nokeEvents };
 export { default as nokeConstants } from './noke-constants';
 export { default as nokeUtils } from './noke-utils';
-
-export const nokeEventUtils = {
-    onEvent,
-    onEventOnce,
-    offEvent,
-    getEventListeners,
-    fromNokeEvents,
-};
 
 const Noke = {
     getConstants: NokeAndroidMobileLibrary.getConstants,
     getName: NokeAndroidMobileLibrary.getName,
     nokeConstants,
     ...nokeUtils,
-    ...nokeEventUtils,
 };
 
 export { NokeAndroidMobileLibrary as NokeAndroid };
