7b6d7dfd4b70297da81f6c32812f782b5fc4bf94
John Hillert <john.hillert@gmail.com>
2021-02-15 04:33:34 -0600

    Added redux-saga

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from '@root-reducer';
+// import mySaga from './sagas'
 
+// FLIPPER (blacklist same as redux-debugger)
+// let reduxDebugger = createDebugger({ stateWhitelist: ['user'] });
+// const actionsBlacklist = ['EVENTS/', 'LOCAL/setClock'];
+// const reduxDebugger = createDebugger({ actionsBlacklist });
 const createDebugger = require('redux-flipper').default;
 
-const store = configureStore({
+// RUDUX SAGA
+const sagaMiddleware = createSagaMiddleware();
+
+
+const storeConfig = {
     reducer: rootReducer,
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(createDebugger()),
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(sagaMiddleware, createDebugger()),
     devTools: process.env.NODE_ENV !== 'production',
-});
+    // preloadedState: ___,
+    // enhancers: ___,
+};
+
+const store = configureStore(storeConfig);
+
+// sagaMiddleware.run(mySaga)
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
     module.hot.accept(() => {
---
+++ src/noke-sagas/noke-sagas.js
@@ -0,0 +1,2 @@
+/* eslint-disable prettier/prettier */
+import { NativeEventEmitter } from 'react-native';
