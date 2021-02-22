5c990b8e0f33ec6e76b0e6ee75c8ccb91bedfbe1
John Hillert <john.hillert@gmail.com>
2021-02-21 22:11:32 -0600

    Added svg support

---
+++ declarations.d.ts
@@ -0,0 +1,7 @@
+// prevents svg not found error
+declare module '*.svg' {
+    import React from 'react';
+    import { SvgProps } from 'react-native-svg';
+    const content: React.FC<SvgProps>;
+    export default content;
+}
