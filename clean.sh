db1ecfcda0df9c25810f4c37bd11040ae22be64c
John Hillert <john.hillert@gmail.com>
2021-02-21 22:03:31 -0600

    Spruced up clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,18 @@
+#!/bin/zsh
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIYellow='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# Color off
+Color_Off='\033[0m'
+
 echo "Delete watchmen"
 watchman watch-del-all
 
@@ -8,27 +23,36 @@ rm -rf  $TEMPDIR/haste-*
 rm -rf  $TEMPDIR/metro-*
 rm -rf  $TEMPDIR/haste-map-react-native-packager-*
 
-echo "Clean Android files"
-cd android && ./gradlew clean && cd..
+echo "${BIYellow}🔸 Cleaning Android files${Color_Off}"
+cd android && ./gradlew clean && cd ..
+echo "${BIGreen}◆ done${Color_Off}"
 
-echo "Clean iOS simulator files"
+echo "${BIYellow}🔸 Cleaning iOS simulator files${Color_Off}"
 rm -rf ~/Library/Developer/Xcode/DerivedData/MemberServices-*
+echo "${BIGreen}◆ done${Color_Off}"
 
-echo "Removing node_modules folder"
+echo "${BIYellow}🔸 Removing node_modules folder${Color_Off}"
 rm -rf node_modules
+echo "${BIGreen}◆ done${Color_Off}"
 
-echo "Removing package-lock.json file"
+echo "${BIYellow}🔸 Removing package-lock.json file${Color_Off}"
 rm -f package-lock.json
+echo "${BIGreen}◆ done${Color_Off}"
 
-echo "Removing pods folder"
+echo "${BIYellow}🔸 Removing pods folder${Color_Off}"
 cd ios && rm -rf pods
+echo "${BIGreen}◆ done${Color_Off}"
 
-echo "Removing Podfile.lock file"
+echo "${BIYellow}🔸 Removing Podfile.lock file${Color_Off}"
 rm -f Podfile.lock && cd ..
+echo "${BIGreen}◆ done${Color_Off}"
 
-echo "NPM - Installing node modules"
+echo "${BIYellow}🔸 Installing node modules (npm)${Color_Off}"
 # npm i
 npm install --force
+echo "${BIGreen}◆ done${Color_Off}"
 
-echo "Cocoapod - Installing pods"
+echo "${BIYellow}🔸 Cocoapod - Installing pods${Color_Off}"
 cd ios && pod install && cd ..
+
+echo "${BIGreen}◆◆◆ DONE ◆◆◆${Color_Off}"
