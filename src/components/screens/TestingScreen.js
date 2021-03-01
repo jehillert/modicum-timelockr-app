5a0cf18db53b575181f59ddcc4daae13ae00808f
John Hillert <john.hillert@gmail.com>
2021-03-01 03:52:54 -0600

    ...

--- src/components/screens/TestingScreen.js
+++ src/components/screens/TestingScreen.js
@@ -11,10 +11,7 @@ function TestingScreen() {
     return (
         <>
             <SafeAreaView>
-                <ScrollView
-                    nestedScrollEnabled={true}
-                    contentInsetAdjustmentBehavior="automatic"
-                    style={styles.scrollView}>
+                <ScrollView nestedScrollEnabled={true} contentInsetAdjustmentBehavior="automatic">
                     {global.HermesInternal == null ? null : (
                         <View style={styles.engine}>
                             <Text style={{ color: 'red', backgroundColor: 'black' }}>Engine: Hermes</Text>
@@ -30,16 +27,12 @@ function TestingScreen() {
 }
 
 const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: Colors.black,
-    },
     engine: {
         position: 'absolute',
         top: 5,
         right: 5,
     },
     sectionContainer: {
-        backgroundColor: Colors.black,
         marginTop: 23,
         paddingHorizontal: 24,
         paddingVertical: 24,
