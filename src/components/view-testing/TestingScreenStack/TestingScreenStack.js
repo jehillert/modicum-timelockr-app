07251bc38d4547ca39678ad02dde1c64f4f8b8e9
John Hillert <john.hillert@gmail.com>
2021-03-14 22:35:52 -0500

    ...

--- src/components/view-testing/TestingScreenStack/TestingScreenStack.js
+++
@@ -1,6 +0,0 @@
-import { TestingScreen } from '@view-testing';
-import { createStackScreen } from '@common';
-
-const TestingScreenStack = createStackScreen(TestingScreen, 'Noke Android API');
-
-export default TestingScreenStack;
--- src/components/view-testing/index.js
+++ src/components/view-testing/index.js
@@ -1,3 +1,2 @@
 export { default as TestingButtons } from './TestingButtons/TestingButtons';
 export { default as TestingScreen } from './TestingScreen/TestingScreen';
-export { default as TestingScreenStack } from './TestingScreenStack/TestingScreenStack';
